40d749e0256049c984840a43c876e2db2b05ee3a
韩俊强 <532167805@qq.com>
2016-06-22 13:46:15 +0800

    提交开源工具类

---
+++ GCTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCTool
+//
+//  Created by HanJunQiang on 16/6/21.
+//  Copyright © 2016年 HaRi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCTool/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  GCTool
+//
+//  Created by HanJunQiang on 16/6/21.
+//  Copyright © 2016年 HaRi. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "GCTool/GCTool.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (IBAction)sendEmail:(id)sender
+{
+    // wifi
+//    BOOL isWifi = [GCTool IsEnableWIFI];
+//    isWifi ? NSLog(@"wifi"):NSLog(@"noWifi");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
